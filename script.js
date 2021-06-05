117d342e3317c883ca5b1e2817185f9caa6148d8
Huy <lsqhuy@gmail.com>
2021-06-05 11:33:54 +0700

    [Test] Test feature request HTML

--- script.js
+++ script.js
@@ -3,12 +3,9 @@
 // Link list => danh sách liên kết
 
 // TREE
-
 console.log("Xin chào");
 
 var htmlTag = document.querySelector("html");
-console.log({ document }, "IM DOCUMENT");
-console.debug(htmlTag, "HTML TAG");
 
 // localstorage, sessionstorage
 const isMatchMediaDarkMode = window.matchMedia(
@@ -17,17 +14,13 @@ const isMatchMediaDarkMode = window.matchMedia(
 
 const isDarkModeFromStorage = localStorage.getItem("isDarkModeOn");
 
-var isDarkModeOn = isDarkModeFromStorage || isMatchMediaDarkMode;
-
-console.log(window.matchMedia("(prefers-color-scheme: dark)"));
+var isDarkModeOn = isDarkModeFromStorage === "true" || isMatchMediaDarkMode;
 
 /**
  * THIS FUNCTION INIT DARKMODE WHEN SCRIPT LOAD
  */
 (function () {
-  console.debug("Init");
   if (isDarkModeOn) {
-    console.log("TOGGLE DARKMODE");
     htmlTag.classList.add("dark-mode");
   }
   localStorage.setItem("isDarkModeOn", isDarkModeOn);
@@ -146,3 +139,74 @@ function toggleDarkMode() {
 // const jbo = obj;
 // console.log(obj);
 // jbo.name = "HHHHHHH";
+
+const header = document.querySelector("header");
+const section = document.querySelector("section");
+let requestURL =
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+let request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+request.onload = function () {
+  const superHeroes = request.response;
+  populateHeader(superHeroes);
+  showHeroes(superHeroes);
+};
+
+fetch(requestURL, { method: "GET" })
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch((error) => console.error("error:", error));
+function populateHeader(obj) {
+  const myH1 = document.createElement("h1");
+  myH1.textContent = obj["squadName"];
+  header.appendChild(myH1);
+
+  const myPara = document.createElement("p");
+  myPara.textContent =
+    "Hometown: " + obj["homeTown"] + " // Formed: " + obj["formed"];
+  header.appendChild(myPara);
+}
+
+const fetchApi = async () => {
+  console.log("FETCH");
+  const response = await fetch(requestURL, { method: "GET" });
+  const json = await response.json()
+  console.log(json, "???12301928")
+};
+
+fetchApi();
+
+function showHeroes(obj) {
+  const heroes = obj["members"];
+
+  for (let i = 0; i < heroes.length; i++) {
+    const myArticle = document.createElement("article");
+    const myH2 = document.createElement("h2");
+    const myPara1 = document.createElement("p");
+    const myPara2 = document.createElement("p");
+    const myPara3 = document.createElement("p");
+    const myList = document.createElement("ul");
+
+    myH2.textContent = heroes[i].name;
+    myPara1.textContent = "Secret identity: " + heroes[i].secretIdentity;
+    myPara2.textContent = "Age: " + heroes[i].age;
+    myPara3.textContent = "Superpowers:";
+
+    const superPowers = heroes[i].powers;
+    for (let j = 0; j < superPowers.length; j++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = superPowers[j];
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
